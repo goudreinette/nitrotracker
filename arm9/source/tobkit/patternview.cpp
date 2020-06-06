fccb883bfe4dd452e9201cb79f03c92a373759b5
Rein van der Woerd <reinvanderwoerd@me.com>
2020-06-07 01:16:09 +0200

    Removed the earlier light occluded lines thing again

--- arm9/source/tobkit/patternview.cpp
+++ arm9/source/tobkit/patternview.cpp
@@ -281,20 +281,11 @@ void PatternView::draw(void)
 	
 		if((realrow>=0)&&(realrow<=ptnlen)) {
 			int effectivewidth = getEffectiveWidth();
-			int nonoccludedmax = 227 - getCellWidth();
-			int fullend = std::fmin(effectivewidth, nonoccludedmax);
-			int remainder = std::fmax(effectivewidth - nonoccludedmax, 0);
 
 			if(realrow%PV_CELL_HEIGHT==0) {
-				// MAIN LINES - full brightness
 				drawLine(0, PV_CELL_HEIGHT*i, effectivewidth, 1, linescol);
-				// faded
-				// drawLine(fullend, PV_CELL_HEIGHT*i, remainder, 1, col_sublines);
 			} else {
-				// SUB LINES - full brightness
 				drawLine(PV_BORDER_WIDTH, PV_CELL_HEIGHT*i, effectivewidth-PV_BORDER_WIDTH, 1, col_sublines);
-				// faded
-				// drawLine(fullend, PV_CELL_HEIGHT*i, remainder, 1, RGB15(28,28,28)|BIT(15));
 			}
 		}
 	}
@@ -302,9 +293,7 @@ void PatternView::draw(void)
 	// V-Lines
 	for(u16 i=0;i<=getNumVisibleChannels();++i) {
 		u16 col = i > 6 ? col_sublines : linescol;
-		// if (i < 7) {
-			drawLine(PV_BORDER_WIDTH-1+i*getCellWidth(), 0, width-PV_BORDER_WIDTH, 0, col);
-		// }
+		drawLine(PV_BORDER_WIDTH-1+i*getCellWidth(), 0, width-PV_BORDER_WIDTH, 0, col);
 	}
 	
 	// Cursor bar
